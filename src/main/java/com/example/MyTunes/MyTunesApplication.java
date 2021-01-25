4c0c80ed878f381c3a2121cb9502ebaaa1793e6f
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-25 14:22:32 +0100

    initial commit

---
+++ src/main/java/com/example/MyTunes/MyTunesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyTunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyTunes/MyTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
