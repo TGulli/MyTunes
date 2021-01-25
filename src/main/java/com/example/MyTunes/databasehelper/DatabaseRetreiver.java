d139e0752156c81ce31e102c6591fd425e7f519a
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-25 14:49:40 +0100

    getting ready for testing

--- src/main/java/com/example/MyTunes/databasehelper/DatabaseRetreiver.java
+++ src/main/java/com/example/MyTunes/databasehelper/DatabaseRetreiver.java
@@ -2,6 +2,8 @@ package com.example.MyTunes.databasehelper;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DatabaseRetreiver {
 
@@ -13,10 +15,31 @@ public class DatabaseRetreiver {
             // Open Connection
             conn = DriverManager.getConnection(URL);
             System.out.println("Connection to SQLite has been established.");
+
+            // Prepare Statement
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT * FROM Customer");
+            // Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Process Results
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+            }
         }
+
+        catch (Exception e){
+                System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                conn.close();
+            }
             catch (Exception e){
                 System.out.println(e.getMessage());
             }
+        }
     }
 
 
