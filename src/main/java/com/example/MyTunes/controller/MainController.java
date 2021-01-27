5ca28ff8cc95e1bcfbbf75b805751313208a1229
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-27 13:27:38 +0100

    activating the branch

--- src/main/java/com/example/MyTunes/controller/MainController.java
+++ src/main/java/com/example/MyTunes/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
+    //hello from the other side
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model){
         model.addAttribute("greeting", "oh my god this works");
