5fc820df20a325ced45a7796a4f7c79310113d7f
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-27 19:41:41 +0100

    added todo

--- src/main/java/com/example/MyTunes/MyTunesApplication.java
+++ src/main/java/com/example/MyTunes/MyTunesApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyTunesApplication {
 
+	/**
+	 * TODO:
+	 * Refactor and delete unused methods
+	 * Fix all the front end stuff
+	 * Task 5 should summarize invoice total from all invoices related to arist, not just top.
+	 * Task 6 needs a entry method for getting id. view-all-customers could for instance have a "Show most payed genres" or something
+	 */
+
+
 	public static void main(String[] args) {
 //		DatabaseRetreiver.task3(new Customer("ERT", "Pedersen", "Norway", "1337", "12345678", "bjon@ped.no"));
 //		DatabaseRetreiver.task1();
