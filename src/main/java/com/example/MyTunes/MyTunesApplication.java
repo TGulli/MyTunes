a39b85f94923b0f33dad29b60a9941d501995630
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-29 15:21:33 +0100

    fixing todo

--- src/main/java/com/example/MyTunes/MyTunesApplication.java
+++ src/main/java/com/example/MyTunes/MyTunesApplication.java
@@ -13,20 +13,11 @@ public class MyTunesApplication {
 	 * Refactor and delete unused methods
 	 * Make frontend pretty (add, edit)
 	 * Needs restcontroller AND controller. Controller works internally with thyme and Restcontroller for exposing the api.
-	 * The database methods should probably return JSON objects.
 	 */
 
 
 	public static void main(String[] args) {
-//		DatabaseRetreiver.task3(new Customer(0, "bajas", "Norway", "1337", "12345678", "bjon@ped.no", "rere2j3"));
-//		DatabaseRetreiver.task1();
-//		DatabaseRetreiver.task4();
-//		DatabaseRetreiver.task5();
-//		DatabaseRetreiver.task6("12");
-		//minipush
-
 		SpringApplication.run(MyTunesApplication.class, args);
-		//Kommer dette??
 	}
 
 
