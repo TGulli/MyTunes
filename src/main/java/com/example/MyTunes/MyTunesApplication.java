f64e968b44462e30070e55d81f1d40d9611ec858
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-30 17:43:12 +0100

    extends

--- src/main/java/com/example/MyTunes/MyTunesApplication.java
+++ src/main/java/com/example/MyTunes/MyTunesApplication.java
@@ -2,9 +2,10 @@ package com.example.MyTunes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyTunesApplication {
+public class MyTunesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTunesApplication.class, args);
