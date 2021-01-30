69ddad96817155e804201c756bf2af169f6e97b9
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-30 19:05:06 +0100

    srsly

--- src/test/java/com/example/MyTunes/MyTunesApplicationTests.java
+++ src/test/java/com/example/MyTunes/MyTunesApplicationTests.java
@@ -9,25 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyTunesApplicationTests {
-	IRepository db = null;
 
-	@BeforeEach
-	public void setup(){
-		db = new SQLiteDatabase();
-	}
 
-	@Test
-	void contextLoads() {
-	}
 
-	@Test
-	public void checkIfDatabaseWorks(){
-		db.createCustomer(new Customer(999999, "hei", "nei", "tjohei", "bay", "buja", "somethign"));
-	}
-
-	@Test
-	public void checkEntriesInDatabase(){
-		System.out.println(db.getAllCustomers());
-	}
 
 }
