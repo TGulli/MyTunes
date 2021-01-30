54532966ddc54aeda31ef47cbd618b431e45985f
Bjørnar Pedersen <bjornar.pedersen@no.experis.com>
2021-01-30 16:58:39 +0100

    testing push to heroku

--- src/main/java/com/example/MyTunes/MyTunesApplication.java
+++ src/main/java/com/example/MyTunes/MyTunesApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyTunesApplication {
-
-	/**
-	 * TODO:
-	 * README
-	 * Docker on Heroku
-	 * Postman tests against the API
-	 */
-
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyTunesApplication.class, args);
 	}
